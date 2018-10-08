9679f48ca81d30c48410003c2a7a97c64e461641
fsk16 <fsk16@skbl-422-009.ph.ic.ac.uk>
2018-10-08 16:11:03 +0100

    Added PORTD as input

--- Simple1.asm
+++ Simple1.asm
@@ -9,13 +9,15 @@
 start
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movlw	0xFF
+	movwf	TRISD, ACCESS	    ; Port D all bits set to 1 for input
+	movlw 	0x0		    ; Sets W back to 0
 	bra 	test
 loop	movff 	0x06, PORTC
-	movlw	0x01
-	addwf	0x06, W		    ; Changed operation inc to add 1
-	;incf 	0x06, W, ACCESS
+	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x14
+	movlw 	0xFF
+	;movf	PORTD, W, ACCESS
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
