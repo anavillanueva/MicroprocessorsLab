959dfaed52f524068d1b1bce6c861188f432513d
fsk16 <fsk16@skbl-422-009.ph.ic.ac.uk>
2018-10-12 12:28:05 +0100

    Built clock

--- Simple1.asm
+++ Simple1.asm
@@ -12,43 +12,19 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	
 	
 	goto	start
-	; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
-	constant 	myArray=0x400	; Address in RAM for data
-	constant 	counter=0x10	; Address of counter variable
-	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-
-	call subroutine
-	decfsz	counter	; count down to zero
-	bra	loop		; keep going until finished
-	; test comment
-	; test branch
-	goto	0
+	; ******* My data and where to put it in RAM 
+start	movlw	0x0
+movwf	TRISE, ACCESS ; Port E all outputs
+	movlw	0x0
+movwf	TRISD, ACCESS ; Port E all outputs
 	
-subroutine  
-	movlw .10
-	movwf 0x20 ; store 10 in 0x20
-subroutine_loop
-	call subsubroutine
-	decfsz 0x20 ; count down from 10 to 0
-	bra subroutine_loop
-	return
-	; test comment commit
-subsubroutine	
-	movlw .3
-	movwf 0x21 ; store 3 in 0x21	
-subsubroutine_loop decfsz 0x21 ; count down from 3 to 0
-	bra subsubroutine_loop
-	return
-	end
+movlw	0x8E
+movwf	0xF84
+	
+loop	movlw   0x01  ; Clock with period of 374ns
+	movwf   PORTD
+	movlw   0x0
+	movwf   PORTD
+	bra loop
+	
+	end 
