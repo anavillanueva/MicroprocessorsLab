ba82adda15da9f29c8a9e5a85cf245e7a8985453
fsk16 <fsk16@skbl-422-009.ph.ic.ac.uk>
2018-10-15 15:43:16 +0100

    1 bit memory, output displays in PORTC

--- Simple1.asm
+++ Simple1.asm
@@ -13,13 +13,23 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	
 	goto	start
 	; ******* My data and where to put it in RAM 
-start	movlw	0x0
-movwf	TRISE, ACCESS ; Port E all outputs
-	movlw	0x0
-movwf	TRISD, ACCESS ; Port E all outputs
+start	clrf TRISE ; Port E all outputs
+	clrf TRISD ; Port D all outputs
+	clrf TRISC ; Port C all outputs
+	movlw   0x03  ; set p0 and p1 to 1
+	movwf   PORTD
+	movlw	0x8E
+	movwf	0xF84
 	
-movlw	0x8E
-movwf	0xF84
+	movlw   0x01  ; Sets clock to low, output still high
+	movwf   PORTD
+	movlw   0x03  ; Clocks data into memory
+	movwf   PORTD
+	setf TRISE ; sets PORTE to tri-state
+	movlw 0x02
+	movwf PORTD ; sets output to low
+	movf PORTE, W 
+	movwf PORTC
 	
 loop	movlw   0x01  ; Clock with period of 374ns
 	movwf   PORTD
