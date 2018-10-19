9e8dc9fa8764be6b970bf493015fb0091eed516e
fsk16 <fsk16@skbl-422-009.ph.ic.ac.uk>
2018-10-19 12:12:45 +0100

    Parallel to serial working in SPI Built a serial to parallel converter with a shift register (74HC164)

--- Simple1.asm
+++ Simple1.asm
@@ -1,53 +1,32 @@
 	#include p18f87k22.inc
-
-	extern	UART_Setup, UART_Transmit_Message  ; external subroutines
 	
-acs0	udata_acs   ; reserve data space in access ram
-counter	    res 1   ; reserve one byte for a counter variable
-delay_count res 1   ; reserve one byte for counter in the delay routine
+	code
+	org 0x0
 
-tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
-myArray res 0x80    ; reserve 128 bytes for message data
 
-rst	code	0    ; reset vector
-	goto	setup
-
-pdata	code    ; a section of programme memory for storing data
-	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
-	constant    myTable_l=.13	; length of data
+SerialtoParallel
+	call SPI_MasterInit
+	movlw 0x2A
+	call SPI_MasterTransmit
+	movlw 0x37
+	call SPI_MasterTransmit
+	bra SerialtoParallel
 	
-main	code
-	; ******* Programme FLASH read Setup Code ***********************
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	goto	start
-	
-	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+SPI_MasterInit ; Set Clock edge to positive
+	bcf SSP2STAT, CKE ; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw (1<<SSPEN)|(1<<CKP)|(0x02)
+	movwf SSP2CON1 ; SDO2 output; SCK2 output
+	bcf TRISD, SDO2
+	bcf TRISD, SCK2
+	return 
 		
-	movlw	myTable_l	; output message to UART
-	lfsr	FSR2, myArray
-	call	UART_Transmit_Message
-
-	goto	$		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-delay	decfsz	delay_count	; decrement until zero
-	bra delay
+	
+SPI_MasterTransmit ; Start transmission of data (held in W) 
+	movwf SSP2BUF 	
+Wait_Transmit ; Wait for transmission to complete 
+	btfss PIR2, SSP2IF 
+	bra Wait_Transmit
+	bcf PIR2, SSP2IF ; clear interrupt flag 
 	return
-
+	
 	end
